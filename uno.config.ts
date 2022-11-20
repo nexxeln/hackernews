25e12d3df3804d8cec3186cfe19a1e4c89384abe
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-20 15:29:03 +0530

    feat: global styles

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig, presetUno, transformerVariantGroup } from "unocss";
+import {
+  defineConfig,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
 
 export default defineConfig({
   presets: [presetUno()],
-  transformers: [transformerVariantGroup()],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
 });
