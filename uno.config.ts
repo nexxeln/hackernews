c18ecea0367f079b54d8acd4446100f6271c56f8
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-20 15:33:56 +0530

    feat: add satoshi font

--- uno.config.ts
+++ uno.config.ts
@@ -1,11 +1,20 @@
 import {
   defineConfig,
   presetUno,
+  presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from "unocss";
 
 export default defineConfig({
-  presets: [presetUno()],
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "fontshare",
+      fonts: {
+        sans: "Satoshi",
+      },
+    }),
+  ],
   transformers: [transformerVariantGroup(), transformerDirectives()],
 });
