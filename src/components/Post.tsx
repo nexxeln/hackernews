6d6f1ebbe794c8769038d4b6c035d8ff922651a7
Shoubhit Dash <shoubhit2005@gmail.com>
2022-11-25 15:51:19 +0530

    get post displayed on post page

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,6 @@
 import { A } from "solid-start";
 import type { Component } from "solid-js";
-import { formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow, formatRFC3339, formatRFC7231 } from "date-fns";
 
 export const PostCard: Component<{
   id: string;
@@ -29,6 +29,36 @@ export const PostCard: Component<{
           <A href={`/p/${props.id}`} class="hover:underline">
             view comments
           </A>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export const Post: Component<{
+  title: string;
+  link: string;
+  // fix this
+  username: string | undefined;
+  createdAt: string;
+}> = (props) => {
+  return (
+    <article class="flex flex-col gap-2">
+      <div class="flex flex-col gap-2">
+        <a
+          href={props.link}
+          target="_blank"
+          class="text-xl font-medium hover:underline"
+        >
+          {props.title}
+        </a>
+
+        <div class="text-neutral-4 text-xs md:text-sm flex gap-2">
+          <span>by {props.username}</span>
+          <span>•</span>
+          <span>{formatRFC7231(new Date(props.createdAt))}</span>
+          <span>•</span>
+          <span>{10} comments</span>
         </div>
       </div>
     </article>
--- src/routes/p/[id].tsx
+++ src/routes/p/[id].tsx
@@ -1,3 +1,30 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Match, Switch } from "solid-js";
+import { useParams } from "solid-start";
+import { Post } from "~/components/Post";
+import { trpc } from "~/utils/trpc";
+
 export default function PostPage() {
-  return <div>hi this is work in progress</div>;
+  const { id } = useParams();
+  const post = trpc.posts.getPostById.useQuery(() => ({ id }));
+
+  return (
+    <Switch>
+      <Match when={post.isLoading}>
+        <p>Loading...</p>
+      </Match>
+
+      <Match when={post.isError}>
+        <p class="text-red-4">Oh no! Something went wrong!</p>
+      </Match>
+      <Match when={post.isSuccess}>
+        <Post
+          title={post.data!.title!}
+          link={post.data!.link!}
+          username={post.data!.User!.displayName!}
+          createdAt={post.data!.createdAt!.toString()}
+        />
+      </Match>
+    </Switch>
+  );
 }
